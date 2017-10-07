4326cac0f136c86dff9027f3df21b11d2ae3df04
Demitri Morgan <demitri@pagerduty.com>
2017-10-06 17:17:56 -0700

    Bug fixes to notifications report script
    - CSV in Python < 3 doesn't support UTF-8 so we have to Ellis-Island all the inputs
    - Typos
    - Writing to a cache file that can be resumed from in case the reporting fails, so that the data doesn't have to be re-fetched from the API, i.e. if trawling log entries.

--- notifications_team_report/notifications_team_report.py
+++ notifications_team_report/notifications_team_report.py
@@ -1,32 +1,45 @@
 #!/usr/bin/env python
 
 import argparse
+import codecs
 import csv
 import datetime
+import json
 import logging
+import pickle
 import sys
 import time
 
 import pdreq
 
-# Memoizing dictionaries
-api = None
-notifs = []
-stats = {}
-teams = {}
+def ascii_keys(data):
+    return dict((k.encode('ascii'), v) for (k, v) in data.items())
 
 def main():
-    global api
+    # Memoizing dictionaries
+    api = None
+    notifs = []
+    stats = {}
+    teams = {}
+
     ap = argparse.ArgumentParser(
         description="Generates notification reports for each team."
     )
     ap.add_argument('-i', '--interval', dest='n_days', type=int, default=30,
         help="Specify a number of days back in history over which to generate\
         the report.")
+    ap.add_argument('-r', '--resume-file', dest='resume_file', default=False,
+        type=argparse.FileType('rb'), help="Read previous ILE data from this\
+        file instead of retrieving it from the API.")
     ap.add_argument('-v', dest='verbosity', action='count', default=0,
         help="Logging verbosity (default: INFO-level messages).")
-    ap.add_argument('token', type=str, help="A v2 REST API key to use for\
-        accessing notifications. A read-only key should suffice.")
+    ap.add_argument('-w', '--write-file', dest='write_file', default=False,
+        type=argparse.FileType('wb'), help="A cache file to use for saving ILE\
+        data that can be resumed from, before generating the reports. Useful\
+        for if there's a massive quantity of ILE data to be saved, as a\
+            safeguard in case something goes wrong in the reporting part.")
+    ap.add_argument('-t', '--token', type=str, help="A v2 REST API key to use\
+        for accessing notifications. A read-only key should suffice.")
     args = ap.parse_args()
     loglevs = ['info', 'critical', 'error', 'warning', 'info', 'debug']
     loglev = min(args.verbosity, 5)
@@ -35,51 +48,59 @@ def main():
         stream=sys.stdout
     )
 
-    api = pdreq.APIConnection(args.token)
-
-    abilities_resp = api.get('/abilities')
-    if abilities_resp.status_code // 100 != 2:
-        logging.critical(
-            'API authentication error (status=%d): %s', 
-            ile_resp.status_code,
-            ile_resp.text
-        )
-        return
-
-    if 'teams' not in abilities_resp.json()['abilities']:
-        logging.error(
-"This account doesn't have the \"teams\" \ ability! Why are you even running \
-this script?"
-        )
-        return
-
-    logging.info("Retrieving notification log entries...")
     now_s = int(time.time())
-    now = datetime.datetime.now() 
-    more = True 
-    offset = 0
-    params = {
-        'total': 'true',
-        'limit': 100,
-        'since': (now-datetime.timedelta(args.n_days)).isoformat(),
-        'until': now.isoformat(),
-        'include[]': ['channels'],
-    }
-    while more:
-        params['offset'] = offset
-        ile_resp = api.get('/log_entries', params=params)
-        ile_bod = ile_resp.json() 
-        logging.info("Retrieved %d/%d log entries", offset, ile_bod['total'])
-        for ile in ile_bod['log_entries']:
-            if 'notify_log_entry' in ile['type']:
-                ile_teams = ile['teams']
-                index = len(notifs)
-                for team in ile_teams:
-                    teams.setdefault(team['summary'],[])
-                    teams[team['summary']].append(index)
-                notifs.append(ile)
-        more = ile_bod['more']
-        offset += 100
+    if args.resume_file:
+        notifs, teams = pickle.load(args.resume_file)
+    else:
+        api = pdreq.APIConnection(args.token)
+
+        abilities_resp = api.get('/abilities', throw=True)
+
+        if 'teams' not in abilities_resp.json()['abilities']:
+            logging.error("This account doesn't have the \"teams\" \ ability! "\
+                "Why are you even running this script?"
+            )
+            return
+
+        logging.info("Retrieving notification log entries...")
+        now = datetime.datetime.now() 
+        more = True 
+        offset = 0
+        params = {
+            'total': 'true',
+            'limit': 100,
+            'since': (now-datetime.timedelta(args.n_days)).isoformat(),
+            'until': now.isoformat(),
+            'include[]': ['channels'],
+        }
+        while more:
+            params['offset'] = offset
+            ile_resp = api.get('/log_entries', params=params)
+            ile_txt = ile_resp.text
+            ile_bod = json.loads(ile_txt, object_hook=ascii_keys) 
+            logging.info(
+                "Retrieved %d/%d log entries", 
+                offset+len(ile_bod['log_entries']), 
+                ile_bod['total']
+            )
+            for ile in ile_bod['log_entries']:
+                if 'notify_log_entry' in ile['type']:
+                    ile_teams = ile['teams']
+                    index = len(notifs)
+                    for team in ile_teams:
+                        teams.setdefault(team['summary'],[])
+                        teams[team['summary']].append(index)
+                    notifs.append(ile)
+                    logging.debug("Adding log entry: ID=%s", ile['id'])
+                else:
+                    logging.debug("Skipping log entry because it's not a "\
+                        "notification log entry, but %s: ID=%s", ile['type'], 
+                        ile['id'])
+            more = ile_bod['more']
+            offset += 100
+        if args.write_file:
+            logging.info("Writing log entry data to file...")
+            pickle.dump((notifs, teams), args.write_file)
 
     # Tally up for each team and write to file
     for (team, ile_indices) in teams.items():
@@ -87,29 +108,44 @@ this script?"
             'email':0, 'phone':0, 'sms':0, 'push':0, 'total':0
         })
         team_file = '%s_%d.csv'%(team, now_s)
+        print team_file
         csvf = open(team_file, 'w')
         notifsw = csv.writer(csvf)
         for index in reversed(ile_indices):
             ile = notifs[index]
-            if 'notification' in ile:
+            channel = ile['channel']
+            if 'notification' in channel and 'user' in ile:
+                notification = channel['notification']
                 notif_type = None
-                if 'push' in ile['notification']['type']:
+                if 'push' in notification['type']:
                     notif_type = 'push'
-                elif ile['notification']['type'] in stats['team']:
-                    notif_type = ile['notification']['type']
+                elif notification['type'] in stats[team]:
+                    notif_type = notification['type']
                 else:
-                    logging.error(("Invalid/unrecognized notification channel "+
-                        "type: %s"), ile['notification']['type'])
-                if notif_type: 
+                    logging.error("Invalid/unrecognized notification channel \
+                        type: %s", notification['type'])
+                if notif_type:
                     stats[team][notif_type] += 1
                     stats[team]['total'] += 1
                     
-                    notifsw.writerow([
+                    print( 
                         ile['created_at'],
                         notif_type,
-                        ile['notification']['address'],
-                        ile['agent']['summary']
+                        notification['address'],
+                        ile['user']['summary']
+                    )
+
+                    notifsw.writerow([
+                        ile['created_at'].encode('ascii', 'ignore'),
+                        notif_type.encode('ascii', 'ignore'),
+                        notification['address'].encode('ascii', 'ignore'),
+                        ile['user']['summary'].encode('ascii', 'ignore')
                     ])
+                logging.debug("Added notification log entry: %s",
+                    str(ile))
+            else:
+                logging.warn("Notification log entry contains no notification "\
+                 " or user! %s", json.dumps(ile, indent=4))
         csvf.close()
         logging.info("Wrote list of notifications for team to %s", team_file)
 
