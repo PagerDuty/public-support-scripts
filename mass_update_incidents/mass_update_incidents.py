d2e7401e8a879c4c303bd38c9666a44f2b63f4ce
Karynn Ikeda <kikeda@pagerduty.com>
2019-09-06 14:23:33 -0700

    Final edits to new logic

--- mass_update_incidents/mass_update_incidents.py
+++ mass_update_incidents/mass_update_incidents.py
@@ -22,7 +22,7 @@ PARAMETERS = {
 def mass_update_incidents(args):
     session = pdpyras.APISession(args.api_key,
         default_from=args.requester_email)
-    session.url = "https://api.cloudoverflow.com"
+        
     if args.user_id:
         PARAMETERS['user_ids[]'] = args.user_id.split(',')
         print("Acting on incidents assigned to user(s): "+args.user_id)
