6c0a277755c4f8b36ea6478eae49f4c2f356f293
Isabella Applen <iapplen@pagerduty.com>
2021-01-12 17:21:34 -0800

    added some code for choosing roles with command line arguments. did not test yet.

--- get_info_on_all_users/get_users_by_role.py
+++ get_info_on_all_users/get_users_by_role.py
@@ -4,16 +4,12 @@ import argparse
 import pdpyras
 import sys
 
-# Get all users' contact methods.
-# Originally by Ryan Hoskin:
-# https://github.com/ryanhoskin/pagerduty_get_all_contact_methods
-
 users = []
 
-def get_users(session):
+def get_users(role, session):
     sys.stdout.write("Listing owner, global admins, and team managers:\n")
     for user in session.iter_all('users'):
-      if user['role'] == 'admin' or user['role'] == 'owner':
+      if user['role'] == role:
     #   sys.stdout.write("User: ")
        users.append(user['name'])
     #   sys.stdout.write("\n")
@@ -39,12 +35,19 @@ def print_users():
 
 
 if __name__ == '__main__':
-    ap = argparse.ArgumentParser(description="Retrieves contact info for all "
-        "users in a PagerDuty account")
+    ap = argparse.ArgumentParser(description="Retrieves all users with the role(s) "
+        "stated in the command line argument")
     ap.add_argument('-k', '--api-key', required=True, help="REST API key")
+    ap.add_argument('-t', '--team-managers', required=False, default=False, action='store_true', help="fetch team managers")
     args = ap.parse_args()
     session = pdpyras.APISession(args.api_key)
-    get_users(session)
-    get_teams(session)
+    roles = sys.argv[2:]
+    for role in roles:
+      get_users(role, session)
+
+    if args.team_managers:   
+      get_managers(session)
+      get_teams(session)
+      
     print_users()
 
