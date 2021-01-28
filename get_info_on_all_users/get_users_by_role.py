b596a3f3169b6a2361847e147335edb0a428b3e9
Isabella Applen <iapplen@pagerduty.com>
2021-01-27 16:29:24 -0800

    got command line arguments working and made standard output pretty

--- get_info_on_all_users/get_users_by_role.py
+++ get_info_on_all_users/get_users_by_role.py
@@ -4,50 +4,48 @@ import argparse
 import pdpyras
 import sys
 
-users = []
+team_managers=[]
 
 def get_users(role, session):
-    sys.stdout.write("Listing owner, global admins, and team managers:\n")
-    for user in session.iter_all('users'):
+    sys.stdout.write("Listing %ss:\n"%role)
+    members = [] 
+    for user in session.iter_all('users'): 
       if user['role'] == role:
-    #   sys.stdout.write("User: ")
-       users.append(user['name'])
-    #   sys.stdout.write("\n")
+        sys.stdout.write(user['name'] + "\n")
+        members.append(user['name'])
+    total_for_role = str(len(members))   
+    sys.stdout.write("Total number of "+role+"s: "+total_for_role)
+    sys.stdout.write("\n-----\n")
 
-def get_managers(team_id, session):
+def get_managers(team_id, session): 
     for member in session.iter_all('teams/%s/members'%team_id):
      if member['role'] == 'manager':
-    #   sys.stdout.write("User: ")
-       users.append(member['user']['summary'])
-    #   sys.stdout.write("\n")
+       sys.stdout.write(member['user']['summary'] + "\n")
+       team_managers.append(member['user']['summary'])
+    
 
 def get_teams(session):
     for team in session.iter_all('teams'):
       get_managers(team['id'], session)
-  
-      #    sys.stdout.write("-----\n")
-
-def print_users():
-  set(users)
-  for user in users:
-    sys.stdout.write(user)
-    sys.stdout.write("\n")
-
+    total_team_managers = str(len(team_managers))
+    sys.stdout.write("Total number of team managers: "+total_team_managers+"\n")
+    
 
 if __name__ == '__main__':
     ap = argparse.ArgumentParser(description="Retrieves all users with the role(s) "
         "stated in the command line argument")
     ap.add_argument('-k', '--api-key', required=True, help="REST API key")
-    ap.add_argument('-t', '--team-managers', required=False, default=False, action='store_true', help="fetch team managers")
+    ap.add_argument('-r', '--roles', required=True, help="roles to be fetched", dest='roles')
     args = ap.parse_args()
     session = pdpyras.APISession(args.api_key)
-    roles = sys.argv[2:]
+    roles = (args.roles).split(',')
     for role in roles:
-      get_users(role, session)
-
-    if args.team_managers:   
-      get_managers(session)
-      get_teams(session)
+      if role == "team_managers":
+        get_teams(session)
+      else:  
+        get_users(role, session)
       
-    print_users()
 
+
+
+# acceptable values for roles: admin,read_only_user,read_only_limited_user,user,limited_user,observer,restricted_access,owner
