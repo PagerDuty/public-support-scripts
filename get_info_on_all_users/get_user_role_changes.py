6b8a047db91a4597dc1cde2534ece9e321e4aced
Lewis Rafuse <lewisrafuse@gmail.com>
2024-05-09 17:22:10 -0400

    Write to CSV.

--- get_info_on_all_users/get_user_role_changes.py
+++ get_info_on_all_users/get_user_role_changes.py
@@ -4,9 +4,11 @@
 # the endpoint for audit records is https://api.pagerduty.com/audit/records
 
 import argparse
+import csv
 import pdpyras
-from dateutil import parser, relativedelta
 from datetime import datetime, timezone
+from dateutil import parser, relativedelta
+from tabulate import tabulate
 
 user_tiers = {
     'full_user': 'Full User',
@@ -25,14 +27,14 @@ user_roles = {
 }
 
 user_role_to_tier = {
-    'owner': 'full_user',
-    'admin': 'full_user',
-    'user': 'full_user',
-    'limited_user': 'full_user',
-    'observer': 'full_user',
-    'restricted_access': 'full_user',
-    'read_only_limited_user': 'stakeholder',
-    'read_only_user': 'stakeholder'
+    user_roles['owner']: 'full_user',
+    user_roles['admin']: 'full_user',
+    user_roles['user']: 'full_user',
+    user_roles['limited_user']: 'full_user',
+    user_roles['observer']: 'full_user',
+    user_roles['restricted_access']: 'full_user',
+    user_roles['read_only_limited_user']: 'stakeholder',
+    user_roles['read_only_user']: 'stakeholder'
 }
 
 actor_types = {
@@ -52,27 +54,14 @@ def get_api_params(since, until, user_id):
     return params
 
 def print_changes(changes, audit_tier_changes):
-    format_string = '%(date)-25s %(id)-10s %(summary)-20.20s %(before_value)-20s %(value)-20s %(actor_id)-10s %(actor_type)-10s %(actor_summary)-20.20s'
-    table_max_width = 145
+    header = header_row(audit_tier_changes)
+    print(tabulate(header + changes, tablefmt='grid'))
 
-    # Print the header row before sorting the actual changes
-    print(format_string % changes[0])
-    print(''.join(['-' for _ in range(table_max_width)]))
-
-    # Pop the header row
-    changes.pop(0)
-    changes = sorted(changes, key=lambda rc: rc['date'])
-
-    # Get human readable representations of the API role/tier values
-    for change in changes:
-        if audit_tier_changes:
-            change['value'] = user_tiers[change['value']]
-            change['before_value'] = user_tiers[change['before_value']]
-        else:
-            change['value'] = user_roles[change['value']]
-            change['before_value'] = user_roles[change['before_value']]
-
-        print(format_string % change)
+def write_changes_to_csv(changes, audit_tier_changes, filename):
+    header = header_row(audit_tier_changes)
+    with open(filename, 'w', newline='') as csvfile:
+        writer = csv.DictWriter(csvfile, fieldnames=header[0].keys())
+        writer.writerows(header + changes)
 
 def header_row(audit_tier_changes):
     header = {
@@ -104,7 +93,7 @@ def get_record_actor(record):
     return {
         'actor_type': actor_types[actor['type']],
         'actor_id': actor['id'],
-        'actor_summary': actor['summary'] if 'summary' in actor else ''
+        'actor_summary': actor.get('summary', '')
     }
 
 def get_role_changes(record):
@@ -116,13 +105,18 @@ def get_role_changes(record):
     field_changes = record.get('details', {}).get('fields', [])
     role_changes = filter(lambda fc: fc['name'] == 'role', field_changes)
 
-    def hydarate_role_change(role_change):
-        role_change.update(record['root_resource'])
+    def format_role_change(role_change):
+        role_change = {
+            'id': record['root_resource']['id'],
+            'summary': record['root_resource'].get('summary', ''),
+            'value': user_roles[role_change['value']],
+            'before_value': user_roles[role_change['before_value']],
+            'date': record['execution_time']
+        }
         role_change.update(get_record_actor(record))
-        role_change['date'] = record['execution_time']
         return role_change
 
-    return list(map(hydarate_role_change, role_changes))
+    return list(map(format_role_change, role_changes))
 
 def get_role_tier_changes(role_changes):
     tier_changes = []
@@ -159,7 +153,7 @@ def chunk_date_range(since, until):
             yield (datetime.isoformat(since), datetime.isoformat(until))
             return
 
-def main(since, until, user_id, audit_tier_changes, session):
+def main(since, until, user_id, audit_tier_changes, filename, session):
     try:
         role_changes = []
         chunked_date_range = chunk_date_range(since, until)
@@ -168,12 +162,12 @@ def main(since, until, user_id, audit_tier_changes, session):
                 record_role_changes = get_role_changes(record)
                 role_changes += record_role_changes
 
-        header = header_row(audit_tier_changes)
-
         changes = get_role_tier_changes(role_changes) if audit_tier_changes else role_changes
-
         if len(changes):
-            print_changes(header + changes, audit_tier_changes)
+            changes = sorted(changes, key=lambda rc: rc['date'])
+            print_changes(changes, audit_tier_changes)
+            if filename:
+                write_changes_to_csv(changes, audit_tier_changes, filename)
         else:
             print(f'No {"tier" if audit_tier_changes else "role"} changes found.')
 
@@ -188,6 +182,7 @@ if __name__ == '__main__':
     ap.add_argument('-u', '--until', required=False, help='end date')
     ap.add_argument('-i', '--user-id', required=False, help='Filter results to a single user ID')
     ap.add_argument('-t', '--audit-tier-changes', action='store_true', help='Audit user role tier changes')
+    ap.add_argument('-f', '--filename', required=False, help='Output file name')
     args = ap.parse_args()
     session = pdpyras.APISession(args.api_key)
 
@@ -196,5 +191,6 @@ if __name__ == '__main__':
         args.until,
         args.user_id,
         args.audit_tier_changes,
+        args.filename,
         session
     )
